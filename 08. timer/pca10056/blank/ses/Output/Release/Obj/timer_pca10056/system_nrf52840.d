d04aea8832848324ec2bdfb6f468133fc85e72ec
Motosawa <aa8764114@gmail.com>
2021-10-20 16:56:21 +0800

    2021/10/20 PM
    看教學影片練習用timer
    雖然大概知道timer是什麼，但是完全不知道程式是在打什麼東西
    我想還要再練習

---
+++ 08. timer/pca10056/blank/ses/Output/Release/Obj/timer_pca10056/system_nrf52840.d
@@ -0,0 +1,26 @@
+Output/Release/Obj/timer_pca10056/system_nrf52840.o: \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52840.c \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52.c \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.62/include/stdint.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.62/include/stdbool.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52840.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840_bitfields.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/compiler_abstraction.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf_peripherals.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52840_peripherals.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf_erratas.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf51_erratas.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf52_erratas.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf53_erratas.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/nrf91_erratas.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52.h \
+ /Users/motosawa/Documents/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/system_nrf52_approtect.h
